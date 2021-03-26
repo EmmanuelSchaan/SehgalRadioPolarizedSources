c1461042960dc6e89bb3465e07bf41d381b62c54
EmmanuelSchaan <emmanuel.schaan@gmail.com>
2021-03-26 15:08:07 -0700

    cleaned submit scripts

--- submit_TQU_IR.sh
+++ submit_TQU_IR.sh
@@ -17,18 +17,5 @@ source ~/python_profile.sh
 
 source ./generate_TQU_IR.sh
 
-#python generate_TQU_IR_cutouts.py "IRgal_S_1"
-#python generate_TQU_IR_cutouts.py "IRgal_S_2"
-#python generate_TQU_IR_cutouts.py "IRgal_S_3"
-#python generate_TQU_IR_cutouts.py "IRgal_S_4"
-#python generate_TQU_IR_cutouts.py "IRgal_S_5"
-#python generate_TQU_IR_cutouts.py "IRgal_S_6"
-#python generate_TQU_IR_cutouts.py "IRgal_S_7"
-#python generate_TQU_IR_cutouts.py "IRgal_S_8"
-#python generate_TQU_IR_cutouts.py "IRgal_S_9"
-#python generate_TQU_IR_cutouts.py "IRgal_S_10"
-#python generate_TQU_IR_cutouts.py "IRgal_S_11"
-## BLAST very high flux population
-#python generate_TQU_IR_cutouts.py "IRBlastPop"
 
 
